0ef91674109227ffbf4e9cd5243b631731410660
lizethjsh <lizeth.hernandez7983@alumnos.udg.mx>
2023-06-02 15:52:21 -0600

    ejercicios de la manzana

---
+++ dosuno.py
@@ -0,0 +1,3 @@
+nombre = input("Ingresa tu nombre: ")
+print(nombre) #Se imprime el valor de la variable "nombre"
+print("Nombre") # Te muestra el texto "nombre" tal cual
---
+++ ejerciciodeuno.py
@@ -0,0 +1,7 @@
+precio = int(input("Ingresa el valor de la manzana: "))
+cantidad = int (input ("Manzanas a comprar: "))
+
+print ("La cantidad a pagar es: ", precio * cantidad)
+print ("El precio de las manzanas es: ", precio)
+print ("La cantidad a pagar es: ", cantidad)
+
